1aa92b133ac32cf0f944ff898fa7ec074e49a063
Scott <scott.vivian@gmail.com>
2014-12-08 19:01:36 +0000

    Fix some issues with the JavaScript

--- qa-tt-layer.php
+++ qa-tt-layer.php
@@ -11,34 +11,35 @@ class qa_html_theme_layer extends qa_html_theme_base
 		qa_html_theme_base::head_script();
 
 		if ($this->forbid_new_tag()) {
-			$js =
-				"<script>\n" .
-				"function qa_tag_verify()\n" .
-				"{\n" .
-				"	var tags = jQuery('#tags').val().split(' ');\n" .
-				"	var alltags = ','+qa_tags_complete+',';\n" .
-				"	if ( jQuery('#tags').siblings('.qa-tag-synonyms-error').length > 0 )\n" .
-				"		return false;\n\n" .
+			$js = array(
+				'<script>',
+				'function qa_tag_verify()',
+				'{',
+				'	var tags = jQuery("#tags").val().split(" ");',
+				'	var alltags = ","+qa_tags_complete+",";',
+				'	var errorShowing = jQuery("#tags").siblings(".qa-tag-synonyms-error").length > 0;',
 
-				"	for ( var i in tags )\n" .
-				"	{\n" .
-				"		if ( tags[i].length > 0 && alltags.indexOf(','+tags[i]+',') == -1 )\n" .
-				"		{\n" .
-				"			var error = '<div style=\"display:none\" class=\"qa-form-tall-error qa-tag-synonyms-error\">The tag \"'+tags[i]+'\" does not exist; you need " . number_format( qa_opt('tagging_tools_rep') ) . " points to create new tags.</div>';\n" .
-				"			jQuery(error).insertAfter('#tags').slideDown('fast').delay(5000).slideUp('fast', function() { jQuery(this).detach() } );\n" .
-				"			return false;\n" .
-				"		}\n" .
-				"	}\n\n" .
+				'	for (var i in tags) {',
+				'		if (tags[i].length > 0 && alltags.indexOf(","+tags[i]+",") == -1) {',
+				'			if (!errorShowing) {',
+				'				var error = "<div style=\\"display:none\\" class=\\"qa-form-tall-error qa-tag-synonyms-error\\">The tag <q>"+tags[i]+"</q> does not exist; you need ' . number_format( qa_opt('tagging_tools_rep') ) . ' points to create new tags.</div>";',
+				'				jQuery(error).insertAfter("#tags").slideDown("fast").delay(5000).slideUp("fast", function() { jQuery(this).detach() } );',
+				'			}',
+				'			qa_hide_waiting(this);',
+				'			return false;',
+				'		}',
+				'	}',
 
-				"	document.ask.submit();\n" .
-				"}\n" .
+				'	document.ask.submit();',
+				'}',
 
-				"$(function(){\n" .
-				"	$('.qa-form-tall-button-ask').on('click', qa_tag_verify);\n" .
-				"});\n" .
-				"</script>";
+				'$(function(){',
+				'	$(".qa-form-tall-button-ask").on("click", qa_tag_verify);',
+				'});',
+				'</script>',
+			);
 
-			$this->output_raw($js);
+			$this->output_raw( implode("\n", $js)."\n" );
 		}
 	}
 
