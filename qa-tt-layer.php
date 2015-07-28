e70b5b9ec7f1818325bf3898d549b4db2214e0db
Scott <scott.vivian@gmail.com>
2015-07-28 22:19:24 +0100

    Extract JS to separate file, use correct tag separators
    Fixes #13.

--- qa-tt-layer.php
+++ qa-tt-layer.php
@@ -11,35 +11,14 @@ class qa_html_theme_layer extends qa_html_theme_base
 		qa_html_theme_base::head_script();
 
 		if ($this->forbid_new_tag()) {
-			$js = array(
-				'<script>',
-				'function qa_tag_verify()',
-				'{',
-				'	var tags = jQuery("#tags").val().split(" ");',
-				'	var alltags = ","+qa_tags_complete+",";',
-				'	var errorShowing = jQuery("#tags").siblings(".qa-tag-synonyms-error").length > 0;',
-
-				'	for (var i in tags) {',
-				'		if (tags[i].length > 0 && alltags.indexOf(","+tags[i]+",") == -1) {',
-				'			if (!errorShowing) {',
-				'				var error = "<div style=\\"display:none\\" class=\\"qa-form-tall-error qa-tag-synonyms-error\\">The tag <q>"+tags[i]+"</q> does not exist; you need ' . number_format( qa_opt('tagging_tools_rep') ) . ' points to create new tags.</div>";',
-				'				jQuery(error).insertAfter("#tags").slideDown("fast").delay(5000).slideUp("fast", function() { jQuery(this).detach() } );',
-				'			}',
-				'			qa_hide_waiting(this);',
-				'			return false;',
-				'		}',
-				'	}',
-
-				'	document.ask.submit();',
-				'}',
-
-				'$(function(){',
-				'	$(".qa-form-tall-button-ask").on("click", qa_tag_verify);',
-				'});',
-				'</script>',
+			$replace = array(
+				'VAR_TAG_SEPARATOR' => qa_opt('tag_separator_comma') ? qa_js(',') : qa_js(' '),
+				'VAR_TAG_REP' => number_format(qa_opt('tagging_tools_rep')),
 			);
+			$js = file_get_contents(__DIR__.'/tag-filter.js');
+			$js = strtr($js, $replace);
 
-			$this->output_raw( implode("\n", $js)."\n" );
+			$this->output_raw($js);
 		}
 	}
 
---
+++ tag-filter.js
@@ -0,0 +1,25 @@
+
+function qa_tag_verify()
+{
+	var tagSeparator = VAR_TAG_SEPARATOR;
+	var tags = jQuery("#tags").val().split(" ");
+	var alltags = ","+qa_tags_complete+",";
+	var errorShowing = jQuery("#tags").siblings(".qa-tag-synonyms-error").length > 0;
+	var errorMsg = '<div class="qa-form-tall-error qa-tag-synonyms-error" style="display:none">The tag <q>"+tags[i]+"</q> does not exist; you need VAR_TAG_REP points to create new tags.</div>';
+
+	for (var i in tags) {
+		if (tags[i].length > 0 && alltags.indexOf(","+tags[i]+",") == -1) {
+			if (!errorShowing) {
+				jQuery(errorMsg).insertAfter("#tags").slideDown("fast").delay(5000).slideUp("fast", function() { jQuery(this).detach() } );
+			}
+			qa_hide_waiting(this);
+			return false;
+		}
+	}
+
+	document.ask.submit();
+}
+
+$(function(){
+	$(".qa-form-tall-button-ask").on("click", qa_tag_verify);
+});
