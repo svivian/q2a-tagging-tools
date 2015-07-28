64216a7b21a33a39df37e10da396405ec58aca88
Scott <scott.vivian@gmail.com>
2015-07-28 22:34:42 +0100

    Case-insensitive tag-matching, better error message UX

--- tag-filter.js
+++ tag-filter.js
@@ -2,16 +2,20 @@
 function qa_tag_verify()
 {
 	var tagSeparator = VAR_TAG_SEPARATOR;
-	var alltags = ","+qa_tags_complete+",";
-	var errorShowing = jQuery("#tags").siblings(".qa-tag-synonyms-error").length > 0;
-	var errorMsg = '<div class="qa-form-tall-error qa-tag-synonyms-error" style="display:none">The tag <q>"+tags[i]+"</q> does not exist; you need VAR_TAG_REP points to create new tags.</div>';
 	var tags = $('#tags').val().split(tagSeparator);
+	var alltags = ','+qa_tags_complete.toLowerCase()+',';
+	var errorShowing = $('#tags').siblings('.qa-tag-synonyms-error').length > 0;
 
 	for (var i in tags) {
-		if (tags[i].length > 0 && alltags.indexOf(","+tags[i]+",") == -1) {
-			if (!errorShowing) {
-				jQuery(errorMsg).insertAfter("#tags").slideDown("fast").delay(5000).slideUp("fast", function() { jQuery(this).detach() } );
+		var tag = tags[i];
+		if (tag.length > 0 && alltags.indexOf(','+tag.toLowerCase()+',') == -1) {
+			var errorMsg = '<div class="qa-form-tall-error qa-tag-synonyms-error" style="display:none">The tag <q>'+tag+'</q> does not exist; you need VAR_TAG_REP points to create new tags.</div>';
+
+			if (errorShowing) {
+				$('.qa-tag-synonyms-error').detach();
 			}
+			$(errorMsg).insertAfter('#tags').slideDown().delay(8000).slideUp('fast', function() { $(this).detach() } );
+
 			qa_hide_waiting(this);
 			return false;
 		}
