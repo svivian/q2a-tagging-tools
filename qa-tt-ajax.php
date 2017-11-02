c2cf14cd6c778a384ea0f0d2ec279b00911c94fc
Scott <scott.vivian@gmail.com>
2017-11-02 17:55:03 +0000

    Save tag updates silently

--- qa-tt-ajax.php
+++ qa-tt-ajax.php
@@ -57,10 +57,59 @@ class qa_tagging_tools_ajax
 		foreach ($questions as $q) {
 			$oldtags = qa_tagstring_to_tags(@$q['tags']);
 			$newtags = qa_tt_helper::convert_tags($oldtags, $synonyms);
-			qa_post_set_content($q['postid'], null, null, null, $newtags, null, null, $userid);
+			$this->qa_post_set_content($q['postid'], null, null, null, $newtags, null, null, $userid);
 		}
 		qa_suspend_event_reports(false);
 
 		echo $count['total'];
+	}
+
+	// TEMPORARY duplicate of qa_post_set_content in order to allow saving silently
+	private function qa_post_set_content($postid, $title, $content, $format = null, $tags = null, $notify = null, $email = null, $byuserid = null, $extravalue = null, $name = null)
+	{
+		$oldpost = qa_post_get_full($postid, 'QAC');
+
+		if (!isset($title))
+			$title = $oldpost['title'];
+
+		if (!isset($content))
+			$content = $oldpost['content'];
+
+		if (!isset($format))
+			$format = $oldpost['format'];
+
+		if (!isset($tags))
+			$tags = qa_tagstring_to_tags($oldpost['tags']);
+
+		if (isset($notify) || isset($email))
+			$setnotify = qa_combine_notify_email($oldpost['userid'], isset($notify) ? $notify : isset($oldpost['notify']),
+				isset($email) ? $email : $oldpost['notify']);
+		else
+			$setnotify = $oldpost['notify'];
+
+		$byhandle = qa_userid_to_handle($byuserid);
+
+		$text = qa_post_content_to_text($content, $format);
+
+		switch ($oldpost['basetype']) {
+			case 'Q':
+				$tagstring = qa_post_tags_to_tagstring($tags);
+				// SV: added parameters to save silently
+				qa_question_set_content($oldpost, $title, $content, $format, $text, $tagstring, $setnotify, $byuserid, $byhandle, null, $extravalue, $name, false, true);
+				break;
+
+			case 'A':
+				$question = qa_post_get_full($oldpost['parentid'], 'Q');
+				// SV: added parameters to save silently
+				qa_answer_set_content($oldpost, $content, $format, $text, $setnotify, $byuserid, $byhandle, null, $question, $name, false, true);
+				break;
+
+			case 'C':
+				$parent = qa_post_get_full($oldpost['parentid'], 'QA');
+				$question = qa_post_parent_to_question($parent);
+				// SV: added parameters to save silently
+				qa_comment_set_content($oldpost, $content, $format, $text, $setnotify, $byuserid, $byhandle, null, $question, $parent, $name, false, true);
+				break;
+		}
 	}
 }
